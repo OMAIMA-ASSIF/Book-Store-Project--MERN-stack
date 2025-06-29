e95a4fc0b4cd00785ee6e16909b4b0b24dc32ba6
Omaima Assif <omaima.assif-etu@etu.univh2c.ma>
2025-06-29 15:22:44 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
-
-export const MongoDBURL = "mongodb+srv://omaimaassifetu:xSOlbRfVAP0Dk4kC@cluster0.86twh9r.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0";
+// I removed my password from here :)
+export const MongoDBURL = "mongodb+srv://omaimaassifetu:<password>C@cluster0.86twh9r.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0";
